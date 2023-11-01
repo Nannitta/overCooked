6c753e0c7b93766a8d5734b9ac96bb38108f3ef3
Nanna <nanaa.gonzalez@gmail.com>
2023-11-01 21:16:48 +0100

    Add createUserUseCase wip

---
+++ src/users/domain/User.ts
@@ -0,0 +1,19 @@
+import type { UUID } from 'crypto';
+
+export interface User {
+  userId: UUID,
+  companyName: string,
+  cif: string,
+  emain: string,
+  password: string,
+  phone: string,
+  address: string,
+  city: string,
+  country: string,
+  province: string,
+  postalCode: string,
+  web: string,
+  role: string,
+  createdAt: Date,
+  modifiedAt?: Date
+}
---
+++ test/unit/users/postUserTest.js
@@ -0,0 +1,15 @@
+/* import test from 'node:test';
+import assert from 'node:assert';
+
+test('create user', async () => {
+  const tryCreateUser = async () => {
+    try {
+      
+      const response = await 
+    } catch (error) {
+      throw new Error();
+    }
+  }
+
+  assert.equal();
+}) */
