eb297adae5d94acafc963ee67931e9fc739697c5
unknown <juan@DESKTOP-78O6N7O>
2024-03-03 03:24:58 +0100

    Add Company Name value object

--- src/Users/application/useCases/CreateUserUseCase.ts
+++ src/Users/application/useCases/CreateUserUseCase.ts
@@ -1,10 +1,45 @@
-import type { User } from '../../domain/entities/User.ts';
-import type { UserRepository } from '../../domain/repositories/UserRepository.ts';
+import { User } from "../../domain/entities/User.ts";
+import type { UserRepository } from "../../domain/repositories/UserRepository.ts";
+import { CompanyName } from "../../domain/valueObjects/CompanyName.ts";
 
 export class CreateUserUseCase {
-  constructor (private readonly userRepository: UserRepository) {}
+  constructor(private readonly userRepository: UserRepository) {}
 
-  execute = async (user: User): Promise<void> => {
+  execute = async (
+    companyName: string,
+    CIF: string,
+    email: string,
+    password: string,
+    phone: string,
+    address: string,
+    city: string,
+    country: string,
+    province: string,
+    postalCode: string,
+    web?: string,
+    role?: string,
+    createdAt?: Date,
+    modifiedAt?: Date,
+    userId?: string
+  ): Promise<void> => {
+    const user = User.create(
+      companyName,
+      CIF,
+      email,
+      password,
+      phone,
+      address,
+      city,
+      country,
+      province,
+      postalCode,
+      web,
+      role,
+      createdAt,
+      modifiedAt,
+      userId
+    );
+
     await this.userRepository.postUser(user);
   };
 }
---
+++ src/Users/domain/valueObjects/CompanyName.ts
@@ -0,0 +1,24 @@
+import { throwError } from "../../../shared/infraestructure/utils/errorHelper.ts";
+
+export class CompanyName {
+  public readonly companyName: string;
+
+  private constructor(name: string) {
+    this.companyName = name;
+  }
+
+  public static create(companyName: string): CompanyName {
+    if (!companyName)
+      throw throwError("El nombre de la empresa es obligatorio.", 403);
+    if (companyName.length < 2 || companyName.length > 100)
+      throw throwError(
+        "El nombre de la empresa debe tener entre 2 y 100 caracteres",
+        403
+      );
+    return new CompanyName(this.format(companyName));
+  }
+
+  private static format(companyName: string): string {
+    return companyName.trim().toLowerCase();
+  }
+}
